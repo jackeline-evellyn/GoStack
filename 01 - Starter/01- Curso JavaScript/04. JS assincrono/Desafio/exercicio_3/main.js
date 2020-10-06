592c9883d74c1fff82ac310bacbd5f5769db4e2d
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2020-10-06 14:15:14 -0300

    Exercicios finalizadps

--- 01 - Starter/01- Curso JavaScript/04. JS assincrono/Desafio/exercicio_3/main.js
+++ 01 - Starter/01- Curso JavaScript/04. JS assincrono/Desafio/exercicio_3/main.js
@@ -12,14 +12,41 @@ function renderRepositories(repositories){
         ulElement.appendChild(liElement);
     }
 }
+function renderLoading(loading){
+    ulElement.innerHTML = "";
 
+    var liElement = document.createElement('li');
+    var textLoading = document.createTextNode('Carregando...');
+
+    liElement.appendChild(textLoading);
+    ulElement.appendChild(liElement);
+
+}
+
+function renderError(loading){
+       ulElement.innerHTML = "";
+
+        var liElement = document.createElement('li');
+        var textError = document.createTextNode("Usuário inexistente");
+
+        liElement.style.color = "#F00";
+
+        liElement.appendChild(textError);
+        ulElement.appendChild(liElement);
+}
 function listRepositories(){
     var user = input.value; //pega  o valor recebido no input
 
     if(!user) return;
 
+    renderLoading();
+
     axios.get('https://api.github.com/users/' + user + '/repos')
     .then(function(response){ 
         renderRepositories(response.data);
     })
+    .catch(function(){
+        renderError();
+    });
+
 }
